ee78367d6403e5e94d4166252a332c651a45616a
Max Haslehner <max.haslehner@lx-media.at>
2024-10-29 20:57:38 +0100

    feat: remove chatbot

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,19 +5,6 @@ import Image from "next/image";
 import Link from "next/link";
 
 const Footer: React.FC<{}> = () => {
-  useEffect(() => {
-    var Tawk_LoadStart = new Date();
-    (function () {
-      var s1 = document.createElement("script"),
-        s0 = document.getElementsByTagName("script")[0];
-      s1.async = true;
-      s1.src = process.env.NEXT_PUBLIC_TAWK_TO_ID ?? "";
-      s1.charset = "UTF-8";
-      s1.setAttribute("crossorigin", "*");
-      s0.parentNode?.insertBefore(s1, s0);
-    })();
-  }, []);
-
   return (
     <div className=" container mx-auto 2xl pt-[50px] pb-8 ">
       <div className="pb-10 justify-center flex ">
@@ -42,9 +29,6 @@ const Footer: React.FC<{}> = () => {
           );
         })}
       </div>
-      <p className="text-gray-300 text-center text-sm">
-        Created with ❤️ using Next.js
-      </p>
     </div>
   );
 };
